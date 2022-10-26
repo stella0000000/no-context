c15e22083d5070775e63ffbaf256ff0909a303dc
stella0000000 <112890821+stella0000000@users.noreply.github.com>
2022-10-26 13:28:04 -0400

    google client (#4)

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const app = express();
+require('dotenv').config();
+
 // Imports the Google Cloud client library
 const language = require('@google-cloud/language');
 
-async function quickstart() {
-  
-    // Instantiates a client
-    const client = new language.LanguageServiceClient();
+// const client = new language.LanguageServiceClient();
+// await client.initialize()
+const options = {
+  key: process.env.GOOGLE_API_KEY
+}
 
-    // The text to analyze
+// console.log(options.credentials)
+
+
+async function auth() {
+  try {
+    const client = new language.LanguageServiceClient({
+      options
+    })
     const text = 'Hello, world!';
-  
     const document = {
       content: text,
       type: 'PLAIN_TEXT',
@@ -19,22 +28,27 @@ async function quickstart() {
     // Detects the sentiment of the text
     const [result] = await client.analyzeSentiment({document: document});
     const sentiment = result.documentSentiment;
-  
+
     console.log(`Text: ${text}`);
     console.log(`Sentiment score: ${sentiment.score}`);
     console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
+  } catch(err) {
+    console.log(err)
   }
+}
+
+auth()
 
 
 app.get("/post", async (req, res) => {
     console.log("Connected to React. Post.");
-    await quickstart();
+    // await quickstart();
     res.redirect("/");
 });
 
 app.post("/post", async (req, res) => {
     console.log("Connected to React");
-    await quickstart();
+    // await quickstart();
     res.redirect("/");
 });
   
