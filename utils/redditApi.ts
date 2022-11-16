f0bc92d0bae509333ae3e58d2826a829b1ae9ec0
Gray-lab <martin.grym@gmail.com>
2022-11-16 13:50:20 -0600

    add api call to get comments for post

--- utils/redditApi.ts
+++ utils/redditApi.ts
@@ -1,3 +1,5 @@
+import { mainModule } from "process";
+
 let snoowrap = require('snoowrap');
 
 const r = new snoowrap({
@@ -7,25 +9,43 @@ const r = new snoowrap({
   refreshToken: process.env.REDDIT_REFRESH_TOKEN!,
 });
 
-async function getComments(postName:string) {
-  //.expandReplies({options:{depth:0}}).map((comment:any) => comment.body)
-  const result = await r.getSubmission(postName).expandReplies({options:{depth:0, limit:5}});
+async function getComments(name:string) {
+  // const result = await r.getSubmission(postName).expandReplies({options:{depth:0, limit:5}});
   // console.log(result)
-}
+  const postId = name.slice(3);
+  const sort = "old";
+  const threaded = false;
+  const uri = `/comments/${postId}?sort=${sort}&threaded=${threaded}`;
 
+  const submission = await r._get({uri: `${uri}`})
+  const listing = submission.comments
+
+  const comment_bodies = new Array();
+  for (let i = 0; i < Math.min(listing.length, 20); i++) {
+    comment_bodies.push(listing[i].body)
+  }
+
+  console.log(comment_bodies)
+}
 
 export async function redditApi(query: string) {
   // r.getHot().map((post:any) => post.title).then(console.log);
-  const response = await r.search({query, sort:'relevance', limit:10}).filter((submission:any) => submission.post_hint === 'image').map((submission:any) => {
+  const response = await r.search({query, sort:'relevance', limit:10, includeNsfw:false}).filter((submission:any) => submission.post_hint === 'image').map((submission:any) => {
     return {
       title:submission.title,
       name:submission.name,
       imageUrl:submission.url_overridden_by_dest,
+      over_18: submission.over_18,
+      subreddit: submission.subreddit_name_prefixed,
+      subreddit_id: submission.subreddit_id,
       };
-    })
+    }) 
 
+    
     const result = response[Math.floor(Math.random()*response.length)];
-    // console.log({ result })
-    // getComments(result.name)
+    // console.log(result.submission)
+    // console.log(result.name)
+    getComments(result.name)
+
     return result
 }
