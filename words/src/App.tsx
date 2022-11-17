b6a1b7b0a296c21b20cb82b0759a8ef7989800f7
Gray-lab <martin.grym@gmail.com>
2022-11-17 11:44:10 -0600

    refactored API calls to work with types

--- words/src/App.tsx
+++ words/src/App.tsx
@@ -1,10 +1,50 @@
+import { useState } from 'react';
 import './App.css';
 // import Reddit from './components/Reddit';
 // import Google from './components/Google';
 import Chatbox from './components/Chatbox';
 import UserInput from './components/UserInput';
 
+type RedditData = {
+  imageUrl: string
+  postTitle: string
+  comments: string
+}
+
+type SentimentData = {
+  commentSentiment: Sentiment
+  storySentiment: Sentiment
+}
+
+type Sentiment = {
+  score: number // positive or negative sentiment
+  magnitude: number // magnitude of the score
+}
+
 function App() {
+
+  const [ query, setQuery ] = useState<string | undefined>(undefined)
+  const [ redditData, setRedditData ] = useState<RedditData | undefined>(undefined)
+  const [ sentimentData, setSentimentData ] = useState<SentimentData | undefined>(undefined)
+
+  const reddit = async (query?: string) => {
+    const redditResponse = await fetch(`/reddit?query=${query}`)
+    const redditData: RedditData = await redditResponse.json()
+    setRedditData(redditData)
+  }
+  
+  const getSentiments = async (query?: string): Promise<void> => {
+
+    const storySentiment = await fetch(`/google_sentiment?query=${query}`)
+    const storyResponse: Sentiment = await storySentiment.json()
+    setSentimentData((prevData: any) => ({ ...prevData, storySentiment: storyResponse}))
+  
+    const commentSentiment = await fetch(`/google_sentiment?query=${redditData?.comments}`)
+    const commentSentimentResponse: Sentiment = await commentSentiment.json()
+    setSentimentData((prevData: any) => ({ ...prevData, commentSentiment: commentSentimentResponse}))
+  
+  }
+
   return (
     <div className="App">
       <Chatbox />
